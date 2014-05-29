59677948631f4c2ecce0f4b5ea5aa42e3d9ae1c7
ececilla <ececilla@server>
2014-05-30 01:14:43 +0200

    extended tests

--- test/piercer_test.js
+++ test/piercer_test.js
@@ -79,6 +79,35 @@ exports['piercer'] = {
     test.expect(8);        
     test.done();
   },
+  "add_proxy_sync 2 params concat": function(test) {
+    
+    var start, end;
+    var mod = { dummy:function(param1, param2){
+      
+      test.equal("foo", param1);
+      test.deepEqual(["bar"], param2);
+      end = 1;
+    }};
+    
+    piercer.add_proxy_sync("dummy",function(param1, param2){
+      
+      test.equal("foo", param1);
+      test.deepEqual(["bar"], param2);
+      start = 1;
+    }).add_proxy_sync("dummy",function(param1, param2){
+      
+      test.equal("foo", param1);
+      test.deepEqual(["bar"], param2);
+      start = start+1;
+    });
+    piercer.inject(mod);
+    
+    mod.dummy("foo",["bar"]);
+    test.equal(2,start);      
+    test.ok(end);
+    test.expect(8);        
+    test.done();
+  },
   "add_proxy_async no params": function(test) {
     
     var start;
@@ -93,6 +122,32 @@ exports['piercer'] = {
     piercer.add_proxy_async("dummy",function(next){      
       start = 1;
       setTimeout(next,1000);
+    });
+    piercer.inject(mod);
+    
+    mod.dummy();        
+  },
+  "add_proxy_async err": function(test) {
+    
+    var start;
+    var mod = { dummy:function(param){
+      
+      
+    }};
+
+    piercer.add_proxy_async("dummy",function(next){      
+      start = 1;
+      setTimeout(function(){
+        next("foobarerr")
+      },1000);
+    });
+
+    piercer.add_proxy_err("dummy",function(err){
+
+      test.equals("foobarerr",err);
+      test.ok(start);          
+      test.expect(2);        
+      test.done();
     });
     piercer.inject(mod);
     
